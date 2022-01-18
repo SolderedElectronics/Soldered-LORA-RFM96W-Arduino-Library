68c1541ff9ad1203120c356a8ce2d0a9e86d4cd5
Goran Jurić <74900926+gogi640@users.noreply.github.com>
2022-01-18 12:29:20 +0100

    First version

---
+++ src/LoRa-SOLDERED.h
@@ -0,0 +1,19 @@
+/**
+ **************************************************
+ *
+ * @file        LoRa-SOLDERED.h
+ * @brief       Header file for sensor specific code.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Goran Juric @ soldered.com
+ ***************************************************/
+
+#ifndef __SENSOR__
+#define __SENSOR__
+
+#include "Arduino.h"
+#include "arduino_lmic.h"
+#include "hal/hal.h"
+
+#endif
