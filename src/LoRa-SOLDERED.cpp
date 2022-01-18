f5bc3f227f42d47f778b0aa7d466b6b4ec6d99b3
Main Push Robot <robot@example.com>
2022-01-18 11:27:57 +0000

    Committing dev changes

--- src/LoRa-SOLDERED.cpp
+++ src/LoRa-SOLDERED.cpp
@@ -1,10 +1,10 @@
 /**
  **************************************************
  *
- * @file        Generic-easyC-SOLDERED.cpp
+ * @file        LoRa-SOLDERED.cpp
  * @brief       Example functions to overload in base class.
  *
  *
  * @copyright GNU General Public License v3.0
- * @authors     @ soldered.com
+ * @authors     Goran Juric @ soldered.com
  ***************************************************/
--- src/LoRa-SOLDERED.h
+++ src/LoRa-SOLDERED.h
@@ -1,17 +1,18 @@
 /**
  **************************************************
  *
- * @file        Generic-easyC-SOLDERED.h
+ * @file        LoRa-SOLDERED.h
  * @brief       Header file for sensor specific code.
  *
  *
  * @copyright GNU General Public License v3.0
- * @authors     @ soldered.com
+ * @authors     Goran Juric @ soldered.com
  ***************************************************/
 
 #ifndef __SENSOR__
 #define __SENSOR__
 
 #include "Arduino.h"
+#include "arduino_lmic.h"
 
 #endif
