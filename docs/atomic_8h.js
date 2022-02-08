7e3485139c09ba960e84f411f005f4a8e876e2d4
Docs Robot <robot@example.com>
2022-02-08 08:43:57 +0000

    Committing documentation changes

--- docs/atomic_8h.js
+++ docs/atomic_8h.js
@@ -1,10 +1,10 @@
 var atomic_8h =
 [
-    [ "__get_primask", "atomic_8h.html#aa6b37a0e01bf5a57b5f6ab9c78677124", null ],
-    [ "__iCliParam", "atomic_8h.html#ab79a5066fd5e472ca74660d57b2fc483", null ],
-    [ "__iCliRetVal", "atomic_8h.html#a9424e191942b2a83a4761b07ffcb515d", null ],
-    [ "__iRestore", "atomic_8h.html#a7c74d18fa95dee4ed48b1de3961077bd", null ],
-    [ "__iSeiParam", "atomic_8h.html#a11a63a65c2d70e6c8bdc3e8e4586b5c2", null ],
-    [ "__iSeiRetVal", "atomic_8h.html#a95ebd57195ead8e0d388824e9f4d8ea4", null ],
-    [ "__set_primask", "atomic_8h.html#a68591646432dbb85f71a089f32609234", null ]
+    [ "__get_primask", "atomic_8h.html#a190eb9b4678c94365b10dc88fb067b7e", null ],
+    [ "__iCliParam", "atomic_8h.html#a84b1e4cb317307ae013275df019ab296", null ],
+    [ "__iCliRetVal", "atomic_8h.html#aad4f04ac3c05694f9187a76e11debc83", null ],
+    [ "__iRestore", "atomic_8h.html#afbfd9d769f67502f036441535375e83c", null ],
+    [ "__iSeiParam", "atomic_8h.html#ad16d6979fe6aebc1f400412e182c8fce", null ],
+    [ "__iSeiRetVal", "atomic_8h.html#ad798b9b33046246aa5e9ae4842004adf", null ],
+    [ "__set_primask", "atomic_8h.html#a8dc26aa740118a8d7878af0bf2506b2c", null ]
 ];
--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,13 +1,13 @@
 var searchData=
 [
   ['_5f_5fcxa_5fpure_5fvirtual_0',['__cxa_pure_virtual',['../_r_h_generic_driver_8cpp.html#a6be7d9ce80c86f5178635fa86c2dd5e7',1,'RHGenericDriver.cpp']]],
-  ['_5f_5fget_5fprimask_1',['__get_primask',['../atomic_8h.html#aa6b37a0e01bf5a57b5f6ab9c78677124',1,'atomic.h']]],
-  ['_5f_5ficliparam_2',['__iCliParam',['../atomic_8h.html#ab79a5066fd5e472ca74660d57b2fc483',1,'atomic.h']]],
-  ['_5f_5ficliretval_3',['__iCliRetVal',['../atomic_8h.html#a9424e191942b2a83a4761b07ffcb515d',1,'atomic.h']]],
-  ['_5f_5firestore_4',['__iRestore',['../atomic_8h.html#a7c74d18fa95dee4ed48b1de3961077bd',1,'atomic.h']]],
-  ['_5f_5fiseiparam_5',['__iSeiParam',['../atomic_8h.html#a11a63a65c2d70e6c8bdc3e8e4586b5c2',1,'atomic.h']]],
-  ['_5f_5fiseiretval_6',['__iSeiRetVal',['../atomic_8h.html#a95ebd57195ead8e0d388824e9f4d8ea4',1,'atomic.h']]],
-  ['_5f_5fset_5fprimask_7',['__set_primask',['../atomic_8h.html#a68591646432dbb85f71a089f32609234',1,'atomic.h']]],
+  ['_5f_5fget_5fprimask_1',['__get_primask',['../atomic_8h.html#a190eb9b4678c94365b10dc88fb067b7e',1,'atomic.h']]],
+  ['_5f_5ficliparam_2',['__iCliParam',['../atomic_8h.html#a84b1e4cb317307ae013275df019ab296',1,'atomic.h']]],
+  ['_5f_5ficliretval_3',['__iCliRetVal',['../atomic_8h.html#aad4f04ac3c05694f9187a76e11debc83',1,'atomic.h']]],
+  ['_5f_5firestore_4',['__iRestore',['../atomic_8h.html#afbfd9d769f67502f036441535375e83c',1,'atomic.h']]],
+  ['_5f_5fiseiparam_5',['__iSeiParam',['../atomic_8h.html#ad16d6979fe6aebc1f400412e182c8fce',1,'atomic.h']]],
+  ['_5f_5fiseiretval_6',['__iSeiRetVal',['../atomic_8h.html#ad798b9b33046246aa5e9ae4842004adf',1,'atomic.h']]],
+  ['_5f_5fset_5fprimask_7',['__set_primask',['../atomic_8h.html#a8dc26aa740118a8d7878af0bf2506b2c',1,'atomic.h']]],
   ['_5ffini_8',['_fini',['../wirish_8cpp.html#a9ce78f31b61c8a3f4330e814839a0219',1,'wirish.cpp']]],
   ['_5finit_9',['_init',['../wirish_8cpp.html#a53578094a6e7647b6a9f9ea86975c4c2',1,'wirish.cpp']]]
 ];
