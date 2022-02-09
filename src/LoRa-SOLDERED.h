d2a69ac34c6867ad7ca8ee6d2c0802328754f54e
Main Push Robot <robot@example.com>
2022-02-09 12:00:15 +0000

    Committing dev changes

--- src/LoRa-SOLDERED.h
+++ src/LoRa-SOLDERED.h
@@ -11,6 +11,18 @@
 
 #include "Arduino.h"
 
+// project-specific definitions
+#define CFG_eu868 1
+//#define CFG_us915 1
+//#define CFG_au915 1
+//#define CFG_as923 1
+// #define LMIC_COUNTRY_CODE LMIC_COUNTRY_CODE_JP      /* for as923-JP; also define CFG_as923 */
+//#define CFG_kr920 1
+//#define CFG_in866 1
+#define CFG_sx1276_radio 1
+//#define LMIC_USE_INTERRUPTS
+
+
 typedef uint8_t bit_t;
 typedef uint8_t u1_t;
 typedef int8_t s1_t;
