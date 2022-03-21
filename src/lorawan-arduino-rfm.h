0dd97cafd0f17d7c0e619d75837e2bdabe9de6f3
Clang Robot <robot@example.com>
2022-03-21 08:25:05 +0000

    Committing clang-format changes

--- src/lorawan-arduino-rfm.h
+++ src/lorawan-arduino-rfm.h
@@ -31,14 +31,14 @@
 #ifndef _LORAWAN_ARDUINO_RFM_H_
 #define _LORAWAN_ARDUINO_RFM_H_
 
-#include <SPI.h>
-#include <Arduino.h>
 #include "AES-128.h"
-#include "Encrypt.h"
-#include "RFM95.h"
-#include "LoRaMAC.h"
-#include "Struct.h"
 #include "Config.h"
+#include "Encrypt.h"
+#include "LoRaMAC.h"
+#include "RFM95.h"
+#include "Struct.h"
+#include <Arduino.h>
+#include <SPI.h>
 
 #if defined(ARDUINO_ESP32_DEV)
 #define LORA_DEFAULT_SPI           SPI
@@ -67,83 +67,82 @@
 
 class LoRaWANClass
 {
-    public:
+  public:
+    LoRaWANClass();
+    ~LoRaWANClass();
 
-        LoRaWANClass();
-        ~LoRaWANClass();
-        
-        bool init(void);
-        bool join(void);
-        void setDeviceClass(devclass_t dev_class);
-        // OTAA credentials
-        void setDevEUI(const char *devEUI_in);
-        void setAppEUI(const char *appEUI_in);
-        void setAppKey(const char *appKey_in);
-        // ABP credentials
-        void setNwkSKey(const char *NwkKey_in);
-        void setAppSKey(const char *ApskKey_in);
-        void setDevAddr(const char *devAddr_in);
-        void sendUplink(char *data, unsigned int len, unsigned char confirm, unsigned char mport);
-        void setDataRate(unsigned char data_rate);
-        void setChannel(unsigned char channel);
-        unsigned char getChannel();
-        unsigned char getDataRate();
-        void setTxPower1(unsigned char power_idx);
-        void setTxPower(int level,txPin_t pinTx);
-        int getRssi();
-        int readData(char *outBuff);
-        bool readAck(void);
-        void update(void);
+    bool init(void);
+    bool join(void);
+    void setDeviceClass(devclass_t dev_class);
+    // OTAA credentials
+    void setDevEUI(const char *devEUI_in);
+    void setAppEUI(const char *appEUI_in);
+    void setAppKey(const char *appKey_in);
+    // ABP credentials
+    void setNwkSKey(const char *NwkKey_in);
+    void setAppSKey(const char *ApskKey_in);
+    void setDevAddr(const char *devAddr_in);
+    void sendUplink(char *data, unsigned int len, unsigned char confirm, unsigned char mport);
+    void setDataRate(unsigned char data_rate);
+    void setChannel(unsigned char channel);
+    unsigned char getChannel();
+    unsigned char getDataRate();
+    void setTxPower1(unsigned char power_idx);
+    void setTxPower(int level, txPin_t pinTx);
+    int getRssi();
+    int readData(char *outBuff);
+    bool readAck(void);
+    void update(void);
 
-        // frame counter
-        unsigned int getFrameCounter();
-        void setFrameCounter(unsigned int FrameCounter);
+    // frame counter
+    unsigned int getFrameCounter();
+    void setFrameCounter(unsigned int FrameCounter);
 
-    private:
-        void randomChannel();
+  private:
+    void randomChannel();
 
-    private:        
-        // Messages
-        unsigned char Data_Tx[MAX_UPLINK_PAYLOAD_SIZE];
-        sBuffer Buffer_Tx;
-        unsigned char Data_Rx[MAX_DOWNLINK_PAYLOAD_SIZE];
-        sBuffer Buffer_Rx;
-        sLoRa_Message Message_Rx;
+  private:
+    // Messages
+    unsigned char Data_Tx[MAX_UPLINK_PAYLOAD_SIZE];
+    sBuffer Buffer_Tx;
+    unsigned char Data_Rx[MAX_DOWNLINK_PAYLOAD_SIZE];
+    sBuffer Buffer_Rx;
+    sLoRa_Message Message_Rx;
 
-        // Declare ABP session
-        unsigned char Address_Tx[4];
-        unsigned char NwkSKey[16];
-        unsigned char AppSKey[16];
-        unsigned int Frame_Counter_Tx;
-        sLoRa_Session Session_Data;
+    // Declare ABP session
+    unsigned char Address_Tx[4];
+    unsigned char NwkSKey[16];
+    unsigned char AppSKey[16];
+    unsigned int Frame_Counter_Tx;
+    sLoRa_Session Session_Data;
 
-        // Declare OTAA data struct
-        unsigned char DevEUI[8];
-        unsigned char AppEUI[8];
-        unsigned char AppKey[16];
-        unsigned char DevNonce[2];
-        unsigned char AppNonce[3];
-        unsigned char NetID[3];
-        sLoRa_OTAA OTAA_Data;
+    // Declare OTAA data struct
+    unsigned char DevEUI[8];
+    unsigned char AppEUI[8];
+    unsigned char AppKey[16];
+    unsigned char DevNonce[2];
+    unsigned char AppNonce[3];
+    unsigned char NetID[3];
+    sLoRa_OTAA OTAA_Data;
 
-        // Declare LoRA settings struct
-        sSettings LoRa_Settings;
-        sRFM_pins LoRa_Pins;
+    // Declare LoRA settings struct
+    sSettings LoRa_Settings;
+    sRFM_pins LoRa_Pins;
 
-        unsigned char drate_common;
+    unsigned char drate_common;
 
-        // Lora Setting Class
-        devclass_t dev_class;
+    // Lora Setting Class
+    devclass_t dev_class;
 
-        // channel mode
-        unsigned char currentChannel;
+    // channel mode
+    unsigned char currentChannel;
 
-        // UART
-        RFM_command_t RFM_Command_Status;
-        rx_t Rx_Status;
+    // UART
+    RFM_command_t RFM_Command_Status;
+    rx_t Rx_Status;
 
-        // ACK reception
-        ack_t Ack_Status;
+    // ACK reception
+    ack_t Ack_Status;
 };
 
 #endif
