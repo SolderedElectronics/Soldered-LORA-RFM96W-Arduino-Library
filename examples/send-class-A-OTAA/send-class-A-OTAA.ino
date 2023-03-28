1d2780916b5ee9fc74b349d0b9d00edad577c22a
Robert Sorić <robert.soric@gmail.com>
2023-03-28 07:44:50 +0200

    Fixed descriptions for ABP and OTAA examples

--- examples/send-class-A-OTAA/send-class-A-OTAA.ino
+++ examples/send-class-A-OTAA/send-class-A-OTAA.ino
@@ -2,16 +2,16 @@
  **************************************************
  *
  * @file        send-class-A-ABP.ino
- * @brief       Example to demonstrate sending using 
+ * @brief       Example to demonstrate sending using
  *				OTAA method which is commonly used. It is needed
- *				to get credentials through TTN (TheThingsNetwork) 
+ *				to get credentials through TTN (TheThingsNetwork)
  *				interface, otherwise this example will not work.
  *
- *				This code is beerware; if you see me (or any other collaborator 
- * 				member) at the local, and you've found our code helpful, 
+ *				This code is beerware; if you see me (or any other collaborator
+ * 				member) at the local, and you've found our code helpful,
  * 				please buy us a round!
  * 				Distributed as-is; no warranty is given.
- *              
+ *
  *
  *
  *
@@ -22,21 +22,56 @@
  *
  ***************************************************/
 
-///                 GND----------GND   (ground in)
-///                 3V3----------3.3V  (3.3V in)
-/// interrupt 0 pin D2-----------DIO0  (interrupt request out)
-///             pin D3-----------DIO1  (interrupt request out)
-///             pin D4-----------DIO2  (interrupt request out)
-///             pin D7-----------DIO5  (interrupt request out)
-///          SS pin D10----------NSS   (CS chip select in)
-///         SCK pin D13----------SCK   (SPI clock in)
-///        MOSI pin D11----------MOSI  (SPI Data in)
-///        MISO pin D12----------MISO  (SPI Data out)
-/// This is pinout for Dasduino Core, if you are using other MCU, use SPI pins
-///and Interrupt pin 0, if Dasduino ConnectPlus is used
-/// (or any other ESP32 board) use pins(SS=27, RST=6, DIO0=32, MISO=33, MOSI=25,SCK=26)
+/**
+ * How to run this example:
+ * 0. Make sure you have a LoRa gateway nearby you can connect to (check ttnmapper.org)
+ * 1. Go to TheThingsNetwork.org, make an account and go to your console, select your region
+ * 2. Create an application, and register an end device
+ * 3. Enter the device specifics manually, select according frequency plan to your region. For LoRaWAN version
+ * use 1.0.2.
+ * 4. Use any custom JoinEUI you want
+ * 5. Copy the generated DevEUI and AppKey here below
+ * 6. Open the created end device and copy the AppAUI as well
+ * 7. Connect your Dasduiuno board to the breakout according to the diagrams here
+ * 8. Upload the sketch and check if your device is sending data to TTN.
+ *
+ */
 
-#define LORAWAN		//Specify that module will be used for LoRaWAN network
+/**
+ * Diagram for Dasduino ConnectPlus:
+ * (Note that the 3v3 version of the breakout is required for Dasduino ConnectPlus)
+ * Breakout                     Dasduino ConnectPlus
+ * GND------------------------->GND
+ * VCC------------------------->3V3
+ * MISO------------------------>IO33
+ * MOSI------------------------>IO25
+ * SCK------------------------->26
+ * CS-------------------------->27
+ * RESET----------------------->IO0
+ * DIO0------------------------>IO32
+ * DIO1------------------------>15
+ * DIO2------------------------>14
+ * DIO5------------------------>13
+ * 
+ * Diagram for Dasduino Core:
+ * (Note that the 5V version of the breakout is required for Dasduino Core)
+ * Breakout                     Dasduino Core
+ * GND------------------------->GND
+ * VCC------------------------->VCC
+ * OE-------------------------->VCC
+ * MISO------------------------>D12
+ * MOSI------------------------>D11
+ * SCK------------------------->D13
+ * CS-------------------------->D10
+ * RESET----------------------->D9
+ * DIO0------------------------>D2
+ * DIO1------------------------>D3
+ * DIO2------------------------>D4
+ * DIO5------------------------>D7
+ * 
+ */
+
+#define LORAWAN // Specify that module will be used for LoRaWAN network
 #include "LoRa-SOLDERED.h"
 
 // OTAA credentials
@@ -44,94 +79,99 @@ const char *devEui = "0000000000000000";
 const char *appEui = "0000000000000000";
 const char *appKey = "00000000000000000000000000000000";
 
-const unsigned long interval = 10000;    // 10 s interval to send message
-unsigned long previousMillis = 0;  // will store last time message sent
-unsigned int counter = 0;     // message counter
+const unsigned long interval = 10000; // 10 s interval to send message
+unsigned long previousMillis = 0;     // will store last time message sent
+unsigned int counter = 0;             // message counter
 
 char myStr[50];
 char outStr[255];
 byte recvStatus = 0;
 
-//Pinout is changed here if necessary
+// Pinout is changed here
 const sRFM_pins RFM_pins = {
-  .CS = 5,
-  .RST = 6,
-  .DIO0 = 2,
-  .DIO1 = 3,
-  .DIO2 = 4,
-  .DIO5 = 7,
+  .CS = 27,
+  .RST = 0,
+  .DIO0 = 32,
+  .DIO1 = 15,
+  .DIO2 = 14,
+  .DIO5 = 13,
 };
 
-void setup() {
-  // Setup loraid access
-  Serial.begin(9600);  //Initialize serial communication with PC
-  while(!Serial);
-  if(!lora.init())	//Initialize lora and check if it is initialized successfully
-  {
-    Serial.println("RFM95 not detected");
-    delay(5000);
-    return;
-  }
+void setup()
+{
+    // Setup loraid access
+    Serial.begin(9600); // Initialize serial communication with PC
+    while (!Serial)
+        ;
+    if (!lora.init()) // Initialize lora and check if it is initialized successfully
+    {
+        Serial.println("RFM95 not detected");
+        delay(5000);
+        return;
+    }
 
-  // Set LoRaWAN Class change CLASS_A or CLASS_C
-  lora.setDeviceClass(CLASS_A);
+    // Set LoRaWAN Class change CLASS_A or CLASS_C
+    lora.setDeviceClass(CLASS_A);
 
-  //The spreading factor controls the chirp rate, and thus controls the speed of data transmission. 
-  //Lower spreading factors mean faster chirps and therefore a higher data transmission rate. 
-  //For every increase in spreading factor, the chirp sweep rate is halved, 
-  //and so the data transmission rate is halved.
-  //Lower spreading factors reduce the range of LoRa transmissions, because they reduce 
-  //the processing gain and increase the bit rate. Changing spreading factor allows the network 
-  //to increase or decrease data rate for each end device at the cost of range.
-  //The network also uses spreading factors to control congestion. Spreading factors are orthogonal,
-  //so signals modulated with different spreading factors and transmitted on the same frequency channel
-  //at the same time do not interfere with each other.
-  //Bandwidth is the difference between the upper and lower frequencies in a continuous band of frequencies. 
-  //It is typically measured in hertz, and depending on context, may specifically refer to passband bandwidth 
-  //or baseband bandwidth.
-  //LoRa uses spreading factors from 7 to 12 and bandwidths 125 kHz (or 500kHz but only for spread factor 7).
-  lora.setDataRate(SF9BW125);
+    // The spreading factor controls the chirp rate, and thus controls the speed of data transmission.
+    // Lower spreading factors mean faster chirps and therefore a higher data transmission rate.
+    // For every increase in spreading factor, the chirp sweep rate is halved,
+    // and so the data transmission rate is halved.
+    // Lower spreading factors reduce the range of LoRa transmissions, because they reduce
+    // the processing gain and increase the bit rate. Changing spreading factor allows the network
+    // to increase or decrease data rate for each end device at the cost of range.
+    // The network also uses spreading factors to control congestion. Spreading factors are orthogonal,
+    // so signals modulated with different spreading factors and transmitted on the same frequency channel
+    // at the same time do not interfere with each other.
+    // Bandwidth is the difference between the upper and lower frequencies in a continuous band of frequencies.
+    // It is typically measured in hertz, and depending on context, may specifically refer to passband bandwidth
+    // or baseband bandwidth.
+    // LoRa uses spreading factors from 7 to 12 and bandwidths 125 kHz (or 500kHz but only for spread factor 7).
+    lora.setDataRate(SF9BW125);
 
-  // set channel to random
-  lora.setChannel(MULTI);
-  
-  // Put OTAA Key and DevAddress here
-  lora.setDevEUI(devEui);
-  lora.setAppEUI(appEui);
-  lora.setAppKey(appKey);
+    // set channel to random
+    lora.setChannel(MULTI);
 
-  // Join procedure
-  bool isJoined;
-  do {
-    Serial.println("Joining...");
-    isJoined = lora.join();
-    
-    //wait for 10s to try again
-    delay(10000);
-  }while(!isJoined);
-  Serial.println("Joined to network");
+    // Put OTAA Key and DevAddress here
+    lora.setDevEUI(devEui);
+    lora.setAppEUI(appEui);
+    lora.setAppKey(appKey);
+
+    // Join procedure
+    bool isJoined;
+    do
+    {
+        Serial.println("Joining...");
+        isJoined = lora.join();
+
+        // wait for 10s to try again
+        delay(10000);
+    } while (!isJoined);
+    Serial.println("Joined to network");
 }
 
-void loop() {
-  // Check interval overflow
-  if(millis() - previousMillis > interval) {
-    previousMillis = millis(); 
+void loop()
+{
+    // Check interval overflow
+    if (millis() - previousMillis > interval)
+    {
+        previousMillis = millis();
 
-    sprintf(myStr, "Counter-%d", counter);   //Format string
+        sprintf(myStr, "Counter-%d", counter); // Format string
 
-    Serial.print("Sending: ");
-    Serial.println(myStr);
-    
-    lora.sendUplink(myStr, strlen(myStr), 0, 1);  //Send data to gateway
-    counter++;
-  }
+        Serial.print("Sending: ");
+        Serial.println(myStr);
 
-  recvStatus = lora.readData(outStr); //Get data that LoRa has received
-  if(recvStatus)
-  {
-    Serial.println(outStr);
-  }
-  
-  // Check Lora RX
-  lora.update();  //Check if LoRa has received anything
+        lora.sendUplink(myStr, strlen(myStr), 0, 1); // Send data to gateway
+        counter++;
+    }
+
+    recvStatus = lora.readData(outStr); // Get data that LoRa has received
+    if (recvStatus)
+    {
+        Serial.println(outStr);
+    }
+
+    // Check Lora RX
+    lora.update(); // Check if LoRa has received anything
 }
